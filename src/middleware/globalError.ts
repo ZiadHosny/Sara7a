32d035d88d778737b81a9c3b21b99ad029ba8ac0
ziad <ziad.hosny@bitspire.ca>
2023-07-23 17:31:25 +0300

    add database and views and middlewares

---
+++ src/middleware/globalError.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express"
+import { getFromEnv } from "../utils/getFromEnv.js"
+
+export const globalError = (err: any, _: Request, res: Response, __: NextFunction) => {
+    const { mode } = getFromEnv()
+
+    if (mode == 'dev') {
+        devMode(err, res)
+    } else {
+        prodMode(err, res)
+    }
+}
+
+const prodMode = (err: any, res: Response) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({ message: err.message })
+}
+
+const devMode = (err: any, res: Response) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({ message: err.message, stack: err.stack })
+}
