831183a704d54908541d68c5d0082a27994866ce
ziad <ziad.hosny@bitspire.ca>
2023-12-13 13:41:10 +0200

    remove dist folder

--- backend/dist/modules/baseRouter.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const baseRouter = express.Router();
-baseRouter.get('/', (_, res) => {
-    res.send('Hello From Sara7a Api!');
-});
-export default baseRouter;
--- backend/dist/modules/user/user.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { emailVerify, signIn, signUp } from "./user.controller.js";
-import { userValidation } from "../../middleware/validation/user.validation.js";
-const userRouter = express.Router();
-userRouter.post('/signup', userValidation, signUp);
-userRouter.post('/signIn', signIn);
-userRouter.get('/verify/:token', emailVerify);
-export default userRouter;
