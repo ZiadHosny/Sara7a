296d49facfd5d02104e573034e493073317fa436
ziad <ziad.hosny@bitspire.ca>
2023-12-13 13:35:13 +0200

    remove unused code

---
+++ backend/dist/modules/baseRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+const baseRouter = express.Router();
+baseRouter.get('/', (_, res) => {
+    res.send('Hello From Sara7a Api!');
+});
+export default baseRouter;
---
+++ backend/dist/utils/email/sendEmail.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+import { getFromEnv } from '../getFromEnv.js';
+import { emailHtml } from './emailHtml.js';
+import { logErrInfoMsg, logSuccessMsg } from '../console/log.js';
+export const sendEmail = ({ userEmail, token, subject }) => {
+    const { user, email, pass, emailService } = getFromEnv();
+    const transporter = nodemailer.createTransport({
+        service: emailService,
+        auth: {
+            user: email,
+            pass
+        }
+    });
+    transporter.sendMail({
+        from: `"${user}" <${email}>`,
+        to: userEmail,
+        subject,
+        html: emailHtml({ token }),
+    }, (err, info) => {
+        if (err) {
+            logErrInfoMsg(err);
+        }
+        else {
+            logSuccessMsg('Message Sended Successfully ' + info.accepted);
+        }
+    });
+};
