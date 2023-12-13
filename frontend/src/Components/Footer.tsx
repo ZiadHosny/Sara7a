3f66a8aa21879e70ba4972b29bc6e2683904a3e6
ziad <ziad.hosny@bitspire.ca>
2023-12-13 13:40:24 +0200

    init frontend project

---
+++ frontend/src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-custom text-white fixed-bottom pt-2">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; 2024 Ziad Hosny</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
